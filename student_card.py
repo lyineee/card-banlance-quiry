0f28bfc87acb3ad55a3b154c3175456e4c611740
lyine <liuzhengyuan@hotmail.com>
2020-10-19 16:33:22 +0800

    Bug fix

--- student_card.py
+++ student_card.py
@@ -5,19 +5,18 @@ import os
 import time
 from json.decoder import JSONDecodeError
 
-import cv2
 import requests as rq
 from redis import StrictRedis
 
 logger = logging.getLogger(__file__)
 
 
-class CardBanlanceQuiry(object):
+class CardBalanceQuiry(object):
 
-    banlance_url = "http://ecardfw.upc.edu.cn:20086/User/GetCardInfoByAccountNoParm"
-    history_url = "http://ecardfw.upc.edu.cn:20086/Report/GetMyBill"
+    balance_url = "https://wvpn.upc.edu.cn/http-20086/77726476706e69737468656265737421f5f4408e23367f1e6b188ae29d51367beb72/User/GetCardInfoByAccountNoParm"
+    history_url = "https://wvpn.upc.edu.cn/http-20086/77726476706e69737468656265737421f5f4408e23367f1e6b188ae29d51367beb72/Report/GetMyBill"
     today = None
-    banlance = None
+    balance = None
     cookies = dict()
 
     def __init__(self, student_id, password, card_no):
@@ -34,39 +33,45 @@ class CardBanlanceQuiry(object):
     def date(self):
         return time.strftime("%Y-%m-%d", time.localtime())
 
-    def quiry_banlance(self, retry=3):
-        ticket = self.get_ticket()
+    def quiry_balance(self, retry=3):
+        cookies = self.get_cookies()
         try:
             for _ in range(retry):
                 res = rq.post(
-                    self.banlance_url,
-                    cookies={"hallticket": ticket},
+                    self.balance_url,
+                    cookies=cookies,
                     data={"json": "ture"},
                 )
-                quiry_json = json.loads(res.json()["Msg"])
+                try:
+                    quiry_json = json.loads(res.json()["Msg"])
+                except json.decoder.JSONDecodeError as e:
+                    logger.info("Wvpn ticket Expired, msg: {}".format(e))
+                    cookies = self.get_cookies(refresh=True)
+                    continue
                 if not res.json()["IsSucceed"]:
-                    self.banlance = (
+                    self.balance = (
                         int(quiry_json["query_card"]["card"][0]["db_balance"]) / 100
                     )
                     break
                 else:
-                    logger.info("Hallticket outdated, try to refresh")
-                    ticket = self.get_ticket(refresh=True)
+                    logger.info("Wvpn connection outdated, try to refresh")
+                    cookies = self.get_cookies(refresh=True)
+
         except Exception as e:
-            logger.warning("quiry banlance error, msg: {}".format(e))
+            logger.warning("quiry balance error, msg: {}".format(e))
             raise
 
-        return self.banlance
+        return self.balance
 
-    def quiry_today(self, retry=3):
-        ticket = self.get_ticket()
+    def quiry_today(self, retry=3):  # Api deprecated
+        cookies = self.get_cookies()
         today = 0
         for _ in range(retry):
             try:
 
                 res = rq.post(
                     self.history_url,
-                    cookies={"hallticket": ticket},
+                    cookies=cookies,
                     data={
                         "sdate": self.date(),
                         "edate": self.date(),
@@ -80,32 +85,44 @@ class CardBanlanceQuiry(object):
                 break
             except JSONDecodeError:
                 # raise ConnectionRefusedError
-                ticket = self.get_ticket(refresh=True)
+                cookies = self.get_cookies(refresh=True)
             except Exception as e:
                 logger.warning("quiry history error, msg: {}".format(e))
-                ticket = self.get_ticket(refresh=True)
+                cookies = self.get_cookies(refresh=True)
                 # raise
         return today
 
-    def get_ticket(self, refresh=False):
+    def get_cookies(self, refresh=False):
         if not refresh:
-            ticket = self.redis.hmget(self.student_id, "hallticket")[0]
-            if ticket:
-                ticket = ticket.decode()
-                logger.debug("Get cache hallticket {}".format(ticket))
+            wengine_vpn_ticketwvpn_upc_edu_cn = self.redis.hmget(
+                self.student_id, "wengine_vpn_ticketwvpn_upc_edu_cn"
+            )[0]
+            if wengine_vpn_ticketwvpn_upc_edu_cn:
+                wengine_vpn_ticketwvpn_upc_edu_cn = (
+                    wengine_vpn_ticketwvpn_upc_edu_cn.decode()
+                )
+                logger.debug(
+                    "Get cache wvpn ticket {}".format(wengine_vpn_ticketwvpn_upc_edu_cn)
+                )
+                cookies = {
+                    "wengine_vpn_ticketwvpn_upc_edu_cn": wengine_vpn_ticketwvpn_upc_edu_cn,
+                }
             else:
-                ticket = self.account.get_cookies()
+                cookies = self.account.get_cookies()
         else:
-            ticket = self.account.get_cookies()
-        return ticket
+            value = self.account.get_cookies()
+            cookies = {
+                "wengine_vpn_ticketwvpn_upc_edu_cn": value,
+            }
+        return cookies
 
     def redis_update(self):
-        balance = self.quiry_banlance()
+        balance = self.quiry_balance()
         today = self.quiry_today()
         timestamp = int(time.time())
-        self.redis.hmset(
+        self.redis.hmset(  # TODO hmset is deprecated
             self.student_id,
-            {"banlance": balance, "today": today, "timestamp": timestamp},
+            {"balance": balance, "today": today, "timestamp": timestamp},
         )
 
 
@@ -115,24 +132,14 @@ def ocr(img_b64, access_token):
     params = {"image": img_b64}
     resp = rq.post(base_url.format(access_token), data=params, headers=headers)
     resp_json = resp.json()
-    if "words_result" in resp_json:
+    if "words_result" in resp_json and resp_json["words_result"] != []:
         return resp_json["words_result"][0]["words"]
 
 
 class Login(object):
 
     headers = {
-        "Host": "ecardfw.upc.edu.cn:20086",
-        "Proxy-Connection": "keep-alive",
-        "Content-Length": "72",
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "X-Requested-With": "XMLHttpRequest",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Origin": "http://ecardfw.upc.edu.cn:20086",
-        "Referer": "http://ecardfw.upc.edu.cn:20086/",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-CN,zh;q=0.9,zh-TW;q=0.8,en-US;q=0.7,en;q=0.6,fi;q=0.5",
+        "Referer": "https://wvpn.upc.edu.cn/http-20086/77726476706e69737468656265737421f5f4408e23367f1e6b188ae29d51367beb72"
     }
 
     def __init__(self, card_no, password, retry=5):
@@ -142,9 +149,12 @@ class Login(object):
         self.retry = retry
         self.cookies = {}
         self.validate_code = None
+        self.wvpn_username = os.getenv("WVPN_USERNAME")
+        self.wvpn_password = os.getenv("WVPN_PASSWORD")
 
         self.input_width = 109
         self.input_height = 41
+        self.host = "https://wvpn.upc.edu.cn/http-20086/77726476706e69737468656265737421f5f4408e23367f1e6b188ae29d51367beb72/"
 
         # Connect to redis
         try:
@@ -155,38 +165,49 @@ class Login(object):
             logger.error("Redis env error, msg:  {}".format(e))
             raise
 
+    def _wvpn_cookies(self, username: str, password: str) -> dict:
+        data = {"username": username, "password": password}
+        resp = rq.post("https://wvpn.upc.edu.cn/do-login", data=data)
+        if resp.json()["success"]:
+            cookies = {
+                "wengine_vpn_ticketwvpn_upc_edu_cn": resp.cookies[
+                    "wengine_vpn_ticketwvpn_upc_edu_cn"
+                ],
+            }
+            logger.info("Successfully login wvpn")
+            return cookies
+        else:
+            logger.warning("Fail login to wvpn")
+
     def _get_cookies(self):
         try:
-            resp = rq.get("http://ecardfw.upc.edu.cn:20086/")
-            self.cookies = {"ASP.NET_SessionId": resp.cookies["ASP.NET_SessionId"]}
+            wvpn_cookies = self._wvpn_cookies(self.wvpn_username, self.wvpn_password)
+            self.cookies = wvpn_cookies
+            return wvpn_cookies
         except Exception as e:
             # raise Exception('error when get cookie')
             logger.info("error when get cookie, msg: {}".format(e))
             raise
 
-    def _get_validate_img(self):
+    def _get_validate_img(self) -> bytes:
         logger.debug("Start get captcha image")
         validate_code_time = str(time.time()).replace(".", "")
-        validate_code_url = (
-            "http://ecardfw.upc.edu.cn:20086/Login/GetValidateCode?time={}".format(
-                validate_code_time
-            )
+        validate_code_url = "{}/Login/GetValidateCode?time={}".format(
+            self.host, validate_code_time
         )
         validate_code_resp = rq.get(validate_code_url, cookies=self.cookies)
         if validate_code_resp.status_code == 200:
-            with open("./validate_code_tmp.jpg", "wb") as f:
-                f.write(validate_code_resp.content)
-                f.flush()
-                validate_img = cv2.imread(f.name)
-            os.remove("./validate_code_tmp.jpg")
+            validate_img = validate_code_resp.content
         else:
             raise Exception("error when get validate image")
         logger.debug("successfully get captcha image")
         return validate_img
 
-    def _get_validate_code_ocr(self, validate_img) -> str:
-        img_bytes = cv2.imencode(".jpg", validate_img)[1].tostring()
-        img_b64 = base64.b64encode(img_bytes)
+    def _get_validate_code_ocr(self, validate_img: bytes) -> str:
+        """
+        validate_img: Matrix format image
+        """
+        img_b64 = base64.b64encode(validate_img)
         result = ocr(
             img_b64,
             self.get_access_token(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET")),
@@ -205,7 +226,7 @@ class Login(object):
         }
         try:
             login_resp = rq.post(
-                "http://ecardfw.upc.edu.cn:20086/Login/LoginBySnoQuery",
+                "{}Login/LoginBySnoQuery".format(self.host),
                 data=post_data,
                 cookies=self.cookies,
                 headers=self.headers,
@@ -213,29 +234,28 @@ class Login(object):
         except Exception as e:
             logger.info("error when login, msg{}".format(e))
             raise
-        # resp_json = json.loads(login_resp.content)
         resp_json = login_resp.json()
         if not resp_json["IsSucceed"]:
             raise Exception("login failed", "captcha_error")
-        # update cookies
-        self.cookies["hallticket"] = login_resp.cookies["hallticket"]
-        self.cookies["username"] = login_resp.cookies["username"]
         return self.cookies
 
-    def get_cookies(self, retry=None):
+    def get_cookies(self, retry=None) -> str:
         if not retry:
             retry = self.retry
         count = 0
         while count <= retry:
             self._get_cookies()
             val_img = self._get_validate_img()
+
             # code = self._get_validate_code(val_img)
             code = self._get_validate_code_ocr(val_img)  # Using ocr
             try:
                 self._login(self.no, self.pwd_base64, code)
             except Exception as e:
-                if e.args[1] == "captcha_error":
-                    logger.warning("captcha error")
+                logger.info("Exception catch when login, msg {}".format(e))
+                if len(e.args) >= 2:
+                    if e.args[1] == "captcha_error":
+                        logger.warning("captcha error")
                 # raise
                 count += 1
             else:
@@ -243,9 +263,16 @@ class Login(object):
         else:
             logger.error("Hit maximum try")
             raise Exception("Maximum try!")
-        self.redis.hmset(self.no, {"hallticket": self.cookies["hallticket"]})
-        logger.debug("Store user {} hallticket in redis".format(self.no))
-        return self.cookies["hallticket"]
+        self.redis.hmset(  # TODO hmset is deprecated
+            self.no,
+            {
+                "wengine_vpn_ticketwvpn_upc_edu_cn": self.cookies[
+                    "wengine_vpn_ticketwvpn_upc_edu_cn"
+                ],
+            },
+        )
+        logger.debug("Store user {} wvpn ticket in redis".format(self.no))
+        return self.cookies["wengine_vpn_ticketwvpn_upc_edu_cn"]
 
     def get_access_token(self, client_id, client_secret):
         base_url = "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id={}&client_secret={}"
@@ -268,15 +295,32 @@ def test():
     password = os.getenv("PASSWORD")
     card_no = os.getenv("CARD_NO")
     logger.info("Student ID is {}, password is {}".format(id, password))
-    test_quiry = CardBanlanceQuiry(id, password, card_no)
-    print(test_quiry.quiry_banlance())
+    test_quiry = CardBalanceQuiry(id, password, card_no)
+    print(test_quiry.quiry_balance())
     print(test_quiry.quiry_today())
+    test_quiry.redis_update()
 
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
 
     load_dotenv()
-    logging.basicConfig(level=logging.DEBUG)
-    logger.setLevel(logging.DEBUG)
+    logging.basicConfig(level=os.getenv("LOG_LEVEL", logging.INFO))
+    logger.setLevel(os.getenv("LOG_LEVEL", logging.INFO))
     test()
+
+""" encrypt example using aescfb
+key='wrdvpnisthebest!'
+iv='wrdvpnisthebest!'
+
+var encrypt = function (text, key, iv) {
+    var textLength = text.length
+    text = textRightAppend(text, 'utf8')
+    var keyBytes = utf8.toBytes(key)
+    var ivBytes = utf8.toBytes(iv)
+    var textBytes = utf8.toBytes(text)
+    var aesCfb = new AesCfb(keyBytes, ivBytes, 16)
+    var encryptBytes = aesCfb.encrypt(textBytes)
+    return hex.fromBytes(ivBytes) + hex.fromBytes(encryptBytes).slice(0, textLength * 2)
+}
+"""
